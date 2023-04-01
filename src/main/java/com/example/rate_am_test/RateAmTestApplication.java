cebcbb7357ee5004631411cd657873b71c6f80d4
idamanukyan <ida_manukyan@edu.aua.am>
2023-04-01 23:44:37 +0400

    Initial commit

---
+++ src/main/java/com/example/rate_am_test/RateAmTestApplication.java
@@ -0,0 +1,38 @@
+package com.example.rate_am_test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateAmTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateAmTestApplication.class, args);
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Desktop\\st-homework\\rate_am_test\\src\\main\\java\\com\\example\\rate_am_test\\drivers\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://rate.am/");
+
+        String title = driver.getTitle();
+        WebElement elementById = driver.findElement(By.id("1"));
+        WebElement elementByName = driver.findElement(By.name("usd"));
+        WebElement elementByClassName = driver.findElement(By.className("Banks"));
+        WebElement elementByCssSelector = driver.findElement(By.cssSelector("selector"));
+
+        elementById.click();
+        if (elementByClassName.isDisplayed()) {
+            elementByClassName.getText();
+        }
+        elementByName.sendKeys("Key to send");
+        elementByCssSelector.clear();
+
+        driver.close();
+    }
+
+}
